eb08842198fa9d8e56167d38ecfb179d0cc9e561
Irina <i.ilchenko.spb@gmail.com>
2025-07-03 00:29:46 +0300

    выделит отрисовку задач в функцию

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -12,6 +12,10 @@ export default class TripPresenter {
   #pointModel = null;
   #listPointsView = new ListPointsView();
 
+  #points = [];
+  #destinations = [];
+  #offers = [];
+
   constructor({container, pointModel}) {
     this.#container = container;
     this.#pointModel = pointModel;
@@ -21,19 +25,32 @@ export default class TripPresenter {
     const tripMainContainer = document.querySelector('.trip-main');
     const filtersContainer = tripMainContainer.querySelector('.trip-controls__filters');
 
-    const points = [...this.#pointModel.points];
-    const destinations = [...this.#pointModel.destinations];
-    const offers = [...this.#pointModel.offers];
+    this.#points = [...this.#pointModel.points];
+    this.#destinations = [...this.#pointModel.destinations];
+    this.#offers = [...this.#pointModel.offers];
 
     render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
     render(new FiltersView(), filtersContainer);
     render(new SortingView(), this.#container);
     render(this.#listPointsView, this.#container);
-    render(new EditPointView(Mode.EDIT, DEFAULT_POINT, destinations, offers), this.#listPointsView.element);
-    render(new EditPointView(Mode.CREATE, points[0], destinations, offers), this.#listPointsView.element);
+    render(new EditPointView(Mode.EDIT, DEFAULT_POINT, this.#destinations, this.#offers), this.#listPointsView.element);
+    render(new EditPointView(Mode.CREATE, this.#points[0], this.#destinations, this.#offers), this.#listPointsView.element);
 
-    for (const point of points) {
-      render(new PointView(point, destinations, offers), this.#listPointsView.element);
+    // for (const point of points) {
+    //   render(new PointView(point, destinations, offers), this.#listPointsView.element);
+    // }
+    // for (let i = 0; i < this.#points.length; i++) {
+    //   render(new PointView(this.#points[i], this.#destinations, this.#offers), this.#listPointsView.element);
+    // }
+
+    for (let i = 0; i < this.#points.length; i++) {
+      this.#renderPointView(this.#points[i], this.#destinations, this.#offers);
     }
+
+  }
+
+  #renderPointView(point, destinations, offers) {
+    const pointView = new PointView(point, destinations, offers);
+    render(pointView, this.#listPointsView.element);
   }
 }
